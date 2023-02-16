463f49401f59feece2d34e91e3f8e295c7a34908
omarben23 <omarbenzarrouk1@gmail.com>
2023-02-16 06:47:11 -0800

    the seventh and last task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 printf("Size of the char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printof("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printof("Size of a long long int:  %zu byte(s)\n", sizeof(long long int));
-printof("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int:  %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
