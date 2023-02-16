9ffb00b42c5ca0af9cc1210576a0e56916fbea49
omarben23 <omarbenzarrouk1@gmail.com>
2023-02-16 05:19:21 -0800

    the sixth tastk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - program that prints using the printf functions
  * Return: 0
@@ -7,4 +8,3 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
