40a8a7d7d86621bdcacc90eee8670671a6bac8a0
omarben23 <omarbenzarrouk1@gmail.com>
2023-02-17 14:26:20 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@ int main(void)
 
 	c = 'a';
 	d = 'A';
-       while
-                (c <= 'z') {
-                        putchar(c);
-                        c++;
-                }
-       while
-                (d <= 'Z') {
-                        putchar(d);
-                        d++;
-                }
-       return (0);
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while 
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
 }
-
